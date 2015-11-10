88091ba5da84d9a70335650f43d4cb08542358b3
Kim Tore Jensen <kimtj@met.no>
2015-11-10 15:40:35 +0100

    Version 2.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ config = {
     'author': 'MET Norway',
     'url': 'https://github.com/metno/python-modelstatus-client',
     'download_url': 'https://github.com/metno/python-modelstatus-client',
-    'version': '1.1.0',
+    'version': '2.0.0',
     'install_requires': ['nose', 'requests', 'python-dateutil', 'httmock'],
     'packages': find_packages(),
     'scripts': [],
