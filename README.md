26c870571819f942a6c46f380306ce09804f66f5
Kim Tore Jensen <kimtj@met.no>
2015-11-12 11:09:45 +0100

    Command-line interface prototype
    Summary: This patch adds a crude command-line interface to Modelstatus. It is self-learning, detecting possible arguments from the schema presented from the server. Usage is demonstrated in the README file.
    
    Test Plan: Test the commands manually.
    
    Reviewers: #modelstatus, havardf
    
    Reviewed By: #modelstatus, havardf
    
    Subscribers: heikoklein
    
    Projects: #ecdiss
    
    Differential Revision: https://phab.met.no/D1176

--- README.md
+++ README.md
@@ -84,3 +84,83 @@ Lastly, you can access the schema to get an idea of how the data model looks lik
 ```
 print api.model_run.schema  # { 'huge': 'dictionary' }
 ```
+
+
+## Command-line utility
+
+The Modelstatus client ships with a handy "swiss army knife" that enables you to read and write remote objects from the command line.
+
+Some examples of usage follow. Below, we create a new service backend object:
+
+```
+$ python modelstatus/cli.py \
+    --username X \
+    --api_key Y \
+    http://localhost:8000 \
+    service_backend create \
+    --name /dev/null \
+    --documentation_url 'https://example.com'
+{
+    "created": "2015-11-10T17:35:17+0000",
+    "documentation_url": "https://example.com",
+    "id": "013f06f9-8cf3-4123-94b1-4206b2807cbd",
+    "modified": "2015-11-10T17:35:17+0000",
+    "name": "/dev/null",
+    "resource_uri": "/api/v1/service_backend/013f06f9-8cf3-4123-94b1-4206b2807cbd/"
+}
+```
+
+Searching by specific parameters. If you omit the parameters, all results will be returned.
+
+```
+$ python modelstatus/cli.py http://localhost:8000 model_run search --reference_time 2015-01-01T12:00:00Z --model 8efab026-434e-4baf-b8ed-d921a1b0b427
+[
+    {
+        "created": "2015-11-10T17:27:13+0000",
+        "id": "1ddb75c9-d5b4-49a8-9c27-79a47dd396b5",
+        "model": "/api/v1/model/8efab026-434e-4baf-b8ed-d921a1b0b427/",
+        "modified": "2015-11-10T17:27:13+0000",
+        "reference_time": "2015-01-01T12:00:00+0000",
+        "resource_uri": "/api/v1/model_run/1ddb75c9-d5b4-49a8-9c27-79a47dd396b5/",
+        "version": 1
+    },
+    {
+        "created": "2015-11-10T17:27:20+0000",
+        "id": "3789bc4b-eeb1-488b-96df-a1b2e680bf27",
+        "model": "/api/v1/model/8efab026-434e-4baf-b8ed-d921a1b0b427/",
+        "modified": "2015-11-10T17:27:20+0000",
+        "reference_time": "2015-01-01T12:00:00+0000",
+        "resource_uri": "/api/v1/model_run/3789bc4b-eeb1-488b-96df-a1b2e680bf27/",
+        "version": 2
+    }
+]
+```
+
+Requesting a specific resource item:
+
+```
+$ python modelstatus/cli.py http://localhost:8000 model get 8efab026-434e-4baf-b8ed-d921a1b0b427
+{
+    "bounding_box": "0,0,0,0",
+    "contact": "/api/v1/person/8e547c76-6639-450d-a077-5dbcabeb3581/",
+    "created": "2015-11-05T11:32:59+0000",
+    "grib_center": "ecmf",
+    "grib_generating_process_id": "145",
+    "grid_resolution": "4000.00000",
+    "grid_resolution_unit": "m",
+    "id": "8efab026-434e-4baf-b8ed-d921a1b0b427",
+    "institution": "/api/v1/institution/a5eb5df1-704c-4450-a7a4-388f7e54c496/",
+    "level": 0,
+    "lft": 1,
+    "modified": "2015-11-10T11:37:58+0000",
+    "name": "Nordic",
+    "parent": null,
+    "prognosis_length": 60,
+    "projection": "/api/v1/projection/c2b3d081-bb4d-4dea-975b-0126bdab6691/",
+    "resource_uri": "/api/v1/model/8efab026-434e-4baf-b8ed-d921a1b0b427/",
+    "rght": 2,
+    "time_steps": 60,
+    "tree_id": 1,
+    "wdb_data_provider": "nordic_roms"
+}
+```
---
+++ modelstatus/cli.py
@@ -0,0 +1,116 @@
+"""
+Command-line interface to the Modelstatus server.
+"""
+
+import json
+import argparse
+import modelstatus.api
+
+
+class Client(object):
+    def __init__(self):
+        self.subparsers = {}
+
+    def setup_basic_parser(self):
+        self.parser = argparse.ArgumentParser(add_help=False)
+        self.parser.add_argument('server', help='Modelstatus server to communicate with')
+        self.parser.add_argument('--help', required=False, action='store_true', help='Print help')
+        self.parser.add_argument('--username', required=False, help='Modelstatus user name')
+        self.parser.add_argument('--api_key', required=False, help='Modelstatus API key')
+
+    def setup_preliminary_parser(self):
+        self.setup_basic_parser()
+        self.parser.add_argument('...', nargs=argparse.REMAINDER)
+
+    def setup_sub_commands(self):
+        self.setup_preliminary_parser()
+        args = self.parser.parse_args()
+        self.api = modelstatus.api.Api(args.server, username=args.username, api_key=args.api_key)
+        self.api._get_schema_from_server()
+        self.main_schema = self.api._schema
+
+        # Create a new parser with all sub-commands
+        self.setup_basic_parser()
+        self.subparser = self.parser.add_subparsers(dest='_subparser')
+        for key in self.main_schema.keys():
+            self.subparsers[key] = self.subparser.add_parser(
+                key,
+                add_help=False,
+                help='Interact with the %s resource' % key,
+                )
+
+    def pprint_json_string(self, json_data):
+        hash_ = json.loads(json_data)
+        self.pprint(hash_)
+
+    def pprint(self, hash_):
+        print json.dumps(hash_, sort_keys=True, indent=4, separators=(',', ': '))
+
+    def args_in_schema(self, args_dict):
+        schema_keys = self.schema['fields'].keys()
+        a = {}
+        for key in args_dict:
+            if args_dict[key] is None:
+                continue
+            # Remote queries may have modifier such as field__gte, whereas the
+            # schema only contains the 'field' part.
+            base_key = key.split('__')[0]
+            if base_key in schema_keys:
+                a[key] = args_dict[key]
+        return a
+
+    def exec_get(self, args_dict):
+        resource = self.collection[args_dict['uuid']]
+        serialized = resource._serialize()
+        self.pprint_json_string(serialized)
+
+    def exec_search(self, args_dict):
+        args = self.args_in_schema(args_dict)
+        qs = self.collection.objects
+        qs.filter(**args)
+        resources = [json.loads(x._serialize()) for x in qs]
+        self.pprint(resources)
+
+    def exec_create(self, args_dict):
+        args = self.args_in_schema(args_dict)
+        resource = self.collection.create()
+        [setattr(resource, key, value) for key, value in args.iteritems()]
+        resource._unserialize()
+        resource.save()
+        serialized = resource._serialize()
+        self.pprint_json_string(serialized)
+
+    def setup_parameters(self, subcommand):
+        self.setup_basic_parser()
+        self.parser.add_argument(subcommand, help='The literal string "%s"' % subcommand)
+        self.collection = getattr(self.api, subcommand)
+        self.schema = self.collection.schema
+        self.subparser = self.parser.add_subparsers(dest='action')
+        self.subparsers = {}
+        for action in ['search', 'create', 'get']:
+            self.subparsers[action] = self.subparser.add_parser(action)
+            self.subparsers[action].set_defaults(func=getattr(self, 'exec_%s' % action))
+        for action in ['search', 'create']:
+            for key, value in self.schema['fields'].iteritems():
+                self.subparsers[action].add_argument('--%s' % key, help=value['help_text'])
+        for action in ['get']:
+            self.subparsers[action].add_argument('uuid', help='The UUID of a %s object' % subcommand)
+
+    def main(self):
+        self.setup_sub_commands()
+        args, unknown = self.parser.parse_known_args()
+        self.setup_parameters(args._subparser)
+        args, unknown = self.parser.parse_known_args()
+        args_dict = vars(args)
+        # This very filthy filter ensures that remote filter extensions are
+        # applied magically, at the cost of breaking local syntax checking.
+        for i in xrange(0, len(unknown), 2):
+            args_dict[unknown[i].strip('--')] = unknown[i+1]
+        if args.help:
+            self.parser.print_help()
+            return
+        args.func(args_dict)
+
+if __name__ == '__main__':
+    client = Client()
+    client.main()
