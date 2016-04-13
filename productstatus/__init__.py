c9afe84b11aff777faa8df9612bafbbd2d04350b
Kim Tore Jensen <kimtj@met.no>
2016-04-13 10:39:13 +0200

    Add helper function to determine whether or not dataset is complete

--- productstatus/__init__.py
+++ productstatus/__init__.py
@@ -2,3 +2,8 @@
 The productstatus Python module is an object-based HTTP REST interface to the
 Productstatus service. Use the `Api` class in the `productstatus.api` module.
 """
+
+def datainstance_has_complete_file_count(datainstance):
+    service_backend_uri = datainstance.servicebackend.resource_uri
+    format_uri = datainstance.format.resource_uri
+    return datainstance.data.productinstance.complete[service_backend_uri][format_uri]['file_count']
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ config = {
     'author': 'MET Norway',
     'url': 'https://github.com/metno/python-productstatus-client',
     'download_url': 'https://github.com/metno/python-productstatus-client',
-    'version': '5.1.0',
+    'version': '5.2.0',
     'install_requires': [
         'nose==1.3.7',
         'requests==2.9.1',
